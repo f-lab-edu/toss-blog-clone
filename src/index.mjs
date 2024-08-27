c8af5190304880eec3a1426a02e485b5e1b6db75
merge <fpel8800@gmail.com>
2024-08-27 22:54:45 +0900

    chore: webpack style-loader, css-loader 설정
    - style-loader, css-loader 의존성을 추가했습니다.
    - 폰트와 아이콘을 프로젝트에 추가했습니다.

--- src/index.mjs
+++ src/index.mjs
@@ -1,9 +1,17 @@
 import join from 'lodash.join';
+import './style.css';
+import Icon from './icon.svg';
 
 function component() {
   const element = document.createElement('div');
 
   element.innerHTML = join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
 
   return element;
 }
