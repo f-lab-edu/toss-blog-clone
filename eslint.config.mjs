b79a5ee1db7c4ab8f0403e6bb87c2651b86d48d7
merge <fpel8800@gmail.com>
2024-08-27 20:31:43 +0900

    chore: typescript 설정
    - eslint 설정에 typescript plugin을 추가했습니다.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,12 @@
 import js from '@eslint/js';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import tsEslint from 'typescript-eslint';
 
 export default [
   js.configs.recommended,
   eslintConfigPrettier,
+  ...tsEslint.configs.strict,
+  ...tsEslint.configs.stylistic,
   {
     rules: {
       'max-params': ['error', 3],
