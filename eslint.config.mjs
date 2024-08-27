0f263b440883a07a4dc8f7470ea6274eff16d9e9
merge <fpel8800@gmail.com>
2024-08-27 20:25:01 +0900

    chore: prettier 설정
    - eslint 설정에 prettier plugin을 추가했습니다.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,13 @@
-import js from "@eslint/js"
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
   js.configs.recommended,
+  eslintConfigPrettier,
   {
     rules: {
       'max-params': ['error', 3],
-      'no-console': "error",
-    }
-  }
-]
+      'no-console': 'error',
+    },
+  },
+];
