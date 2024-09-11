722a076fab5a358ab0869684f68d9938b21d8d7c
merge <fpel8800@gmail.com>
2024-09-11 14:53:11 +0900

    feat: 컨텐츠 영역 동적 렌더링 작업
    - 기존 HTML 파일 내에서 그려지고 있던 Contents 영역의 HTML 요소가 동적으로 렌더링 되도록 작업을 진행했습니다.
    - HTML의 요소를 동적으로 렌더링하기 때문에 기존 HTML:contents--main 요소 안에 작성되어있던 부분을 제거했습니다.

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,13 @@ import './style.css'
 import './header.css'
 import './content.css'
 import './footer.css'
+import view from './view';
+
+const contents = document.querySelector('.contents--main');
+
+window.requestAnimationFrame(() => {
+  if (contents) {
+    const newContent = view(contents);
+    contents.replaceWith(newContent);
+  }
+})
