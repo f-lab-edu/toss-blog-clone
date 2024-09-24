d2eab140ca5751202ea9fd01cdd75911f08cbf8c
merge <fpel8800@gmail.com>
2024-09-25 02:04:31 +0900

    feat: View Class 작업 - render, getTemplate
    - View Class를 작업했습니다.
    - Controller 측에서 View를 받아와 render를 직접 호출하여 원하는 UI가 렌더링 되도록 작업을 진행하고 있습니다.
    
    - 고민 중인 문제는 event 바인딩인데 이벤트 바인딩은 UI가 렌더링이 되고 난 뒤에 동작해야 합니다.
    - 그런데 Controller 측에서 작성하는 이벤트 즉, model의 도움이 필요한 로직은 어떻게 처리할지 고민이 필요합니다.

--- src/type/View.ts
+++ src/type/View.ts
@@ -1,6 +1,31 @@
 abstract class View {
+  private readonly target: Element | null;
 
-  abstract render(): string
+  constructor(target: Element | null) {
+    this.target = target
+  }
+
+  abstract getTemplate(): string;
+
+  render() {
+    if (!this.target) {
+      throw Error('The target is not available.');
+    }
+
+    const container = this.target.cloneNode(true);
+
+    const tempContainer = document.createElement('div');
+    tempContainer.innerHTML = this.getTemplate();
+
+    if (!tempContainer.firstChild) {
+      throw Error('The element to render is not available.');
+    }
+
+    container.appendChild(tempContainer.firstChild);
+    this.target.replaceWith(container);
+
+    // this.eventBinding && this.eventBinding();
+  }
 }
 
 export default View
