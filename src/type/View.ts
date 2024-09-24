d56027d199a158da63c552dd591ecbbf000cc896
merge <fpel8800@gmail.com>
2024-09-25 06:22:36 +0900

    feat: View Class Prop 제네릭 추가

--- src/type/View.ts
+++ src/type/View.ts
@@ -1,20 +1,20 @@
-abstract class View {
+abstract class View<Props = undefined> {
   private readonly target: Element | null;
 
   constructor(target: Element | null) {
     this.target = target
   }
 
-  abstract getTemplate(): string;
+  abstract getTemplate(props?: Props): string;
 
-  render() {
+  render(props?:Props) {
     if (!this.target) {
       throw Error('The target is not available.');
     }
 
     const container = this.target.cloneNode(true);
     const tempContainer = document.createElement('div');
-    tempContainer.innerHTML = this.getTemplate();
+    tempContainer.innerHTML = this.getTemplate(props);
 
     if (!tempContainer.firstChild) {
       throw Error('The element to render is not available.');
