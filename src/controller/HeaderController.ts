516f8e2da8802adf46f4b2d429d5863ef00517de
merge <fpel8800@gmail.com>
2024-09-25 13:20:37 +0900

    feat: HeaderController 작업
    - HeaderController를 통해서 HeaderView가 렌더링 되도록 작업했습니다.

---
+++ src/controller/HeaderController.ts
@@ -0,0 +1,32 @@
+import Controller from '../type/Controller';
+import HeaderView from '../view/layout/HeaderView';
+import ButtonView from '../view/ButtonView';
+import { move } from '../view/icons';
+
+class HeaderController extends Controller {
+  private headerView: HeaderView;
+  private slashButtonView: ButtonView;
+  private simplicityButtonView: ButtonView;
+  private subscribeButtonView: ButtonView;
+  private careerButtonView: ButtonView;
+
+  constructor(className: string) {
+    super();
+    this.headerView = new HeaderView(className);
+    this.slashButtonView = new ButtonView(".header--menu");
+    this.simplicityButtonView = new ButtonView(".header--menu");
+    this.subscribeButtonView = new ButtonView(".header--menu");
+    this.careerButtonView = new ButtonView(".header--menu");
+  }
+
+  render() {
+    this.headerView.render();
+
+    this.slashButtonView.render({ content: "SLASH", variant: 'white', suffix: move, color: 'gray' });
+    this.simplicityButtonView.render({ content: "SIMPLICITY", variant: 'white', suffix: move, color: 'gray' });
+    this.subscribeButtonView.render({ content: "구독하기" });
+    this.careerButtonView.render({ content: "채용 바로가기", variant: 'secondary', color: 'gray' });
+  }
+}
+
+export default HeaderController
