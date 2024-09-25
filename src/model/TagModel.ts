1195e953aefa0bbb0c03ed072c1c538f31bb81aa
merge <fpel8800@gmail.com>
2024-09-25 16:45:39 +0900

    feat: TagModel 작업
    - 해당 class를 통해서 controller가 데이터에 접근합니다.

---
+++ src/model/TagModel.ts
@@ -0,0 +1,11 @@
+import Model from '../type/Model';
+import { TAG_DUMMY } from '../dummy';
+
+class TagModel extends Model {
+  constructor() {
+    super();
+    this.data = TAG_DUMMY
+  }
+}
+
+export default TagModel
