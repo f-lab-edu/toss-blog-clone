1b0b8470c7e63223bd8b3785e7bdbec5433169a9
merge <fpel8800@gmail.com>
2024-09-25 15:42:30 +0900

    feat: Trending Item Dummy 데이터 추가

---
+++ src/dummy.ts
@@ -0,0 +1,9 @@
+import { TrendItem } from './view/AsideListView';
+
+export const trendingDummy: TrendItem[] = [{
+  title: "비효율이 있는 곳엔 플랫폼 디자이너가 있다.",
+  author: "이정현"
+}, {
+  title: "토스가 꿈꾸는 React Native 기술의 미래",
+  author: "박서진"
+}]
