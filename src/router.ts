df592dabc4c4fea9a5458178ce6458441007b938
merge <fpel8800@gmail.com>
2024-10-02 16:37:50 +0900

    fix: popstate 이벤트 동작 오류 수정
    - document에 이벤트를 바인딩하여 정상동작하지 않았습니다.

--- src/router.ts
+++ src/router.ts
@@ -28,7 +28,7 @@ const pathToRegex = (path: string) => {
 
 export const createRouter = (init: InitRoute[]) => {
   let query: Record<string, string> = {};
-  document.addEventListener('popstate', routeCheck);
+  window.addEventListener('popstate', routeCheck);
   routeCheck();
 
   function push(path: string) {
